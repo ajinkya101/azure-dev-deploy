91534564e0df334b104a2cd74b77ecf48c66c8d0
ajinkya101 <bhabalajinkya@gmail.com>
2024-02-18 12:54:02 +0530

    fix: update location variable

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "rg01" {
 module "stg01" {
   source               = "git::https://github.com/ajinkya101/azure-storage-account-module.git?ref=v0.0.1"
   rg_name              = var.rg_name
-  region               = var.region
+  location             = var.region
   storage_account_name = var.storage_account_name
   network_rules        = var.network_rules
   tags                 = var.tags
