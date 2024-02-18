168d991c6e285f3f646b10dc99160632c4043bb3
ajinkya101 <bhabalajinkya@gmail.com>
2024-02-18 11:52:53 +0530

    fix: skip checkov module source checks

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@
 #------------------------------
 module "rg01" {
   source = "git::https://github.com/ajinkya101/azure-rg-module.git?ref=v0.0.1"
+  #checkov:skip=CKV_TF_1 "Ensure Terraform module sources use a commit hash" Not Required
 
   rg_name = var.rg_name
   region  = var.region
