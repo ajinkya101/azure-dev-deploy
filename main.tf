d1b927bdc7d3cd1756dcc3bd8467b1bbad87dc29
ajinkya101 <bhabalajinkya@gmail.com>
2024-02-18 12:56:50 +0530

    update checkov skip comment

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 #------------------------------
 # Create a Azure resource group
 #------------------------------
-#Skipping Chcekov checks
-#checkov:skip=CKV_TF_1 "Ensure Terraform module sources use a commit hash" Not Required
 
 module "rg01" {
-  source  = "git::https://github.com/ajinkya101/azure-rg-module.git?ref=v0.0.1"
+  source = "git::https://github.com/ajinkya101/azure-rg-module.git?ref=v0.0.1"
+  #checkov:skip=CKV_TF_1 "Ensure Terraform module sources use a commit hash" Not Required
   rg_name = var.rg_name
   region  = var.region
   tags    = var.tags
 }
 
 module "stg01" {
-  source               = "git::https://github.com/ajinkya101/azure-storage-account-module.git?ref=v0.0.1"
+  source = "git::https://github.com/ajinkya101/azure-storage-account-module.git?ref=v0.0.1"
+  #checkov:skip=CKV_TF_1 "Ensure Terraform module sources use a commit hash" Not Required
   rg_name              = var.rg_name
   location             = var.region
   storage_account_name = var.storage_account_name
